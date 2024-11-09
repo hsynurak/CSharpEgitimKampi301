5a09be0feb2f7a9ac434400669c3b171d2b6822c
huseyinurak <huseyinurak@gmail.com>
2024-11-09 11:58:09 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Conrete/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Conrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+/*
+ Field-Variable-Property
+ int x; --> Direkt olarak sınıf içine tanımlanırsa FIELD
+ int x; --> Class içinde sonunda get, set metotları varsa PROPERTY
+ int x; --> Bir metot içinde tanımlanırsa VARIABLE
+*/
